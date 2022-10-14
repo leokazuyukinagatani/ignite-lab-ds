f27cb890f4c7291571d2c31f42dd9360ab942a3b
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-10-14 14:33:24 -0300

    feat: Button and TextInput

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,15 +13,22 @@ module.exports = {
       '2xl': 32
     },
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-500': '#7C7C8A',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
-      'cyan-500': '#61DAFB',
-      'cyan-200': '#64FFF6',
-      'black': '#000000',
+      transparent: 'transparent',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        500: '#7C7C8A',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+      cyan: {
+        500: '#61DAFB',
+        200: '#00D0E0',
+      },
+      black: '#000000',
+      white: '#FFFFFF',
+
     },
     extend: {
       fontFamily: {
